71cb20c25195bef22d95b97ae8e8f859abb02976
PioKl <kosiu342@gmail.com>
2020-05-16 13:19:55 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
