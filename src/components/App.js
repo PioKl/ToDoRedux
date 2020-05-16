010b8844efa67680589119e895383bf7ec572526
PioKl <kosiu342@gmail.com>
2020-05-16 22:17:03 +0200

    App.js| Add components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../style/App.scss';
+import AddTask from './AddTask';
+import TaskList from './TaskList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      <main>
+        <AddTask />
+        <TaskList />
+      </main>
     </div>
   );
 }
