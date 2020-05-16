6970ecb06cb26502142cfed4feb9c2008c0f6daf
PioKl <kosiu342@gmail.com>
2020-05-16 22:17:34 +0200

    Task.js| Component for single task

---
+++ src/components/Task.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { deleteTask } from "../actions/task.actions";
+import { completeTask } from "../actions/task.actions";
+const Task = ({ task, deleteTask, completeTask }) => {
+    const { title, description, id, completed } = task;
+
+    const handleCheck = () => {
+        completeTask(id)
+    }
+    const handleDelete = () => {
+        deleteTask(id)
+    }
+    return (
+        <div>
+            <li style={{ textDecoration: completed ? "line-through" : "none" }} onClick={handleCheck}>{title}</li>
+            <p>{description}</p>
+            <button onClick={handleDelete}>X</button>
+        </div>
+
+    );
+}
+const mapDispatchToProps = dispatch => ({
+    deleteTask: (id) => dispatch(deleteTask(id)),
+    completeTask: (id) => dispatch(completeTask(id))
+});
+
+export default connect(null, mapDispatchToProps)(Task);
+
+//export default Task;
