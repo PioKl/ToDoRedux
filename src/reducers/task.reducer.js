3acbf417dce2ccd0249e3748bde1226125ee9df3
PioKl <kosiu342@gmail.com>
2020-05-16 22:13:48 +0200

    Task Reducer (working add, delete and complete task)

---
+++ src/reducers/task.reducer.js
@@ -0,0 +1,32 @@
+
+const tasks = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_TASK":
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    title: action.title,
+                    description: action.description,
+                    completed: action.completed,
+                }
+            ];
+        case "REMOVE_TASK":
+            return state.filter(task => task.id !== action.id)
+        case "COMPLETE_TASK":
+            return state.map(task => {
+                return {
+                    /*id: task.id,
+                     title: task.title,
+                     description: task.description, */
+                    ...task, //wszystko co znajduje sie w obiekcie zamiast przepisywac jak powyzej
+                    completed: task.id === action.id ? !task.completed : task.completed
+                }
+            })
+        case "SEARCH_TASK":
+            return state.filter(task => task.title.includes(action.title))
+        default:
+            return state;
+    }
+};
+export default tasks;
