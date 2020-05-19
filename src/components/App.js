8aa7fe047d8546058da915a0301b3092634b5cef
PioKl <kosiu342@gmail.com>
2020-05-19 22:20:33 +0200

    App.js| Add Filters component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../style/App.scss';
 import AddTask from './AddTask';
 import TaskList from './TaskList';
+import Filters from './Filters';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
       </header>
       <main>
+        <Filters />
         <AddTask />
         <TaskList />
       </main>
