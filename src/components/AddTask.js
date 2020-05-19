015d9d83b029005f3db1706a9331941ca3120ca2
PioKl <kosiu342@gmail.com>
2020-05-19 22:24:00 +0200

    AddTask.js| Add categories list to add into reducer by action

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,35 +2,46 @@ import React, { useState, useEffect } from 'react';
 import { connect } from "react-redux";
 import { addTask } from "../actions/task.actions";
 
-import { searchTask } from "../actions/task.actions";
+/* import { searchTask } from "../actions/task.actions"; */
 
 const AddTask = ({ addTask, searchTask }) => {
+    const normal = "normal";
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [search, setSearch] = useState('');
+    const [category, setCategory] = useState(normal);
+    /* const [search, setSearch] = useState(''); */
 
     const handleTitle = (e) => {
         setTitle(e.target.value);
     }
     const handleAddTask = (e) => {
         e.preventDefault();
-        addTask(title, description);
+        if (title !== "") {
+            addTask(title, description, category);
+            setTitle("");
+            setDescription("");
+        }
     }
 
-    const handleSearch = (e) => {
-        setSearch(e.target.value)
-    }
-
-    useEffect(() => {
-        console.log("Dziala", search)
-        searchTask(search)
-    })
+    /*     const handleSearch = (e) => {
+            setSearch(e.target.value)
+        }
+    
+        useEffect(() => {
+            console.log("Dziala", search)
+            searchTask(search)
+        }) */
     return (
         <div>
-            <input type="text" value={search} onChange={handleSearch} />
+            {/* <input type="text" value={search} onChange={handleSearch} /> */}
             <form onSubmit={handleAddTask}>
-                <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" required />
+                <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
                 <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
+                <select id="categories">
+                    <option defaultValue="selected" onClick={(e) => setCategory(e.target.value)} value={normal}>Normal</option>
+                    <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
+                    <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
+                </select>
                 <input type="submit" value="add task" />
             </form>
         </div>
@@ -38,8 +49,8 @@ const AddTask = ({ addTask, searchTask }) => {
 }
 
 const mapDispatchToProps = dispatch => ({
-    addTask: (title, description) => dispatch(addTask(title, description)),
-    searchTask: (search) => dispatch(searchTask(search))
+    addTask: (title, description, category) => dispatch(addTask(title, description, category)),
+    /* searchTask: (search) => dispatch(searchTask(search)) */
 });
 
 export default connect(
