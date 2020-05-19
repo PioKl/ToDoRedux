126f026d55940586ddb762dda7c9ab880639eeb3
PioKl <kosiu342@gmail.com>
2020-05-19 22:21:50 +0200

    index.js| Add filters, filterByCategory reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import tasks from "./task.reducer";
+import filters from "./filter.reducer";
+import filterByCategory from "./filterByCategory.reducer";
 
 export default combineReducers({
-    tasks,
+    tasks, filters, filterByCategory,
 });
