ce88eca9805c8701350f46fd6de90954445ebbf5
PioKl <kosiu342@gmail.com>
2020-05-19 22:27:24 +0200

    filter.actions.js| Now is there only 3 actions showAll, showCompleted, showNotCompleted rest are commented (testing)

---
+++ src/actions/filter.actions.js
@@ -0,0 +1,35 @@
+/* 1 sposob */
+export const showAll = (filter) => ({
+    type: "SHOW_ALL",
+    filter: filter,
+})
+
+export const showCompleted = (filter) => ({
+    type: "SHOW_COMPLETED",
+    filter: filter,
+})
+
+export const showNotCompleted = (filter) => ({
+    type: "SHOW_NOTCOMPLETED",
+    filter: filter,
+})
+
+/* export const showByCategory = (filter) => ({
+    type: "SHOW_BYCATEGORY",
+    filter: filter,
+})
+
+
+ */
+/* 2 sposob */
+/* export const chooseFilter = (filter) => ({
+    type: "FILTER",
+    filter: filter,
+}) */
+
+
+/////////////////////////
+/* export const Filterss = {
+    SHOW_ALL: "SHOW ALL",
+    SHOW_COMPLETED: "SHOW COMPLETED",
+} */
