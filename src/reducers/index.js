780eab55f0615b0d61ca2f25a6ee0639d8e1db07
PioKl <kosiu342@gmail.com>
2020-05-20 19:13:46 +0200

    index.js| Add new reducer searchs to combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import tasks from "./task.reducer";
 import filters from "./filter.reducer";
 import filterByCategory from "./filterByCategory.reducer";
+import searchs from "./filterSearch.reducer";
 
 export default combineReducers({
-    tasks, filters, filterByCategory,
+    tasks, filters, filterByCategory, searchs
 });
