2ac363742b52dc75f28ef7c63ffd0fe675beb66d
PioKl <kosiu342@gmail.com>
2020-05-20 19:10:53 +0200

    AddTask.js|Add new category (shopping) to choose from select panel, also deleting unnecessary comments

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,15 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from "react-redux";
 import { addTask } from "../actions/task.actions";
-
-/* import { searchTask } from "../actions/task.actions"; */
 
 const AddTask = ({ addTask, searchTask }) => {
     const normal = "normal";
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState(normal);
-    /* const [search, setSearch] = useState(''); */
 
     const handleTitle = (e) => {
         setTitle(e.target.value);
@@ -22,23 +19,14 @@ const AddTask = ({ addTask, searchTask }) => {
             setDescription("");
         }
     }
-
-    /*     const handleSearch = (e) => {
-            setSearch(e.target.value)
-        }
-    
-        useEffect(() => {
-            console.log("Dziala", search)
-            searchTask(search)
-        }) */
     return (
         <div>
-            {/* <input type="text" value={search} onChange={handleSearch} /> */}
             <form onSubmit={handleAddTask}>
                 <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
                 <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
                 <select id="categories">
                     <option defaultValue="selected" onClick={(e) => setCategory(e.target.value)} value={normal}>Normal</option>
+                    <option onClick={(e) => setCategory(e.target.value)} value="shooping">Shooping</option>
                     <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
                     <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
                 </select>
@@ -50,7 +38,6 @@ const AddTask = ({ addTask, searchTask }) => {
 
 const mapDispatchToProps = dispatch => ({
     addTask: (title, description, category) => dispatch(addTask(title, description, category)),
-    /* searchTask: (search) => dispatch(searchTask(search)) */
 });
 
 export default connect(
