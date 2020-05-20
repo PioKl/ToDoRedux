fa47114b2e4350db91752517e9d549fe5f05e557
PioKl <kosiu342@gmail.com>
2020-05-20 19:56:59 +0200

    TaskList.js| Conditions improvement (new filterByCategory - shopping and tasks can be now filter also by search value)

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Task from './Task';
-import { AVAILABLE_FILTERS } from '../actions/task.actions'; //do 2 sposobu
-const TaskList = ({ tasks, filters, filterByCategory }) => {
-    console.log(filters)
-    console.log(tasks)
-    console.log(filterByCategory)
+const TaskList = ({ tasks }) => {
     return (
         <div>
             <ul>
-                {/*                 {filters === "SHOW_ALL" ? tasks.map(task => {
-                    return (<Task key={task.id} task={task} />)
-                }) : tasks.map(task => {
-                    if (task.completed === true) {
-                        return <Task key={task.id} task={task} />
-                    } else {
-                        return null
-                    }
-                })} */}
-                {/*                 {tasks.map(task => {
-                    return (<Task key={task.id} task={task} />)
-                })} */}
                 {tasks.map(task => (
                     <Task key={task.id} task={task} />
                 ))}
@@ -30,95 +14,63 @@ const TaskList = ({ tasks, filters, filterByCategory }) => {
     );
 }
 
-/* 1 sposob */
-const filterTodos = (tasks, filters, filterByCategory) => {
-    console.log(filters)
+const filterTasks = (tasks, filters, filterByCategory, searchs) => {
     if (filters === "SHOW_ALL") {
-        //console.log(filters)
-        //return tasks.filter(task => task);
         if (filterByCategory === "normal") {
-            return tasks.filter(task => task.category === "normal" && task.title.includes("a"))
+            return tasks.filter(task => task.category === "normal" && task.title.toLowerCase().includes(searchs.toLowerCase()))
+        }
+        else if (filterByCategory === "shooping") {
+            return tasks.filter(task => task.category === "shooping" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "food") {
-            return tasks.filter(task => task.category === "food")
+            return tasks.filter(task => task.category === "food" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "training") {
-            return tasks.filter(task => task.category === "training")
+            return tasks.filter(task => task.category === "training" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else {
-            return tasks.filter(task => task);
+            return tasks.filter(task => task && task.title.toLowerCase().includes(searchs.toLowerCase()));
         }
     }
     else if (filters === "SHOW_NOTCOMPLETED") {
         if (filterByCategory === "allCategories") {
-            return tasks.filter(task => task.completed === false)
+            return tasks.filter(task => task.completed === false && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "normal") {
-            return tasks.filter(task => task.completed === false && task.category === "normal")
+            return tasks.filter(task => task.completed === false && task.category === "normal" && task.title.toLowerCase().includes(searchs.toLowerCase()))
+        }
+        else if (filterByCategory === "shooping") {
+            return tasks.filter(task => task.completed === false && task.category === "shooping" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "food") {
-            return tasks.filter(task => task.completed === false && task.category === "food")
+            return tasks.filter(task => task.completed === false && task.category === "food" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "training") {
-            return tasks.filter(task => task.completed === false && task.category === "training")
+            return tasks.filter(task => task.completed === false && task.category === "training" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
-        //return tasks.filter(task => task.completed === false);
     }
 
     else if (filters === "SHOW_COMPLETED") {
-        //return tasks.filter(task => task.completed === true);
         if (filterByCategory === "allCategories") {
-            return tasks.filter(task => task.completed === true)
+            return tasks.filter(task => task.completed === true && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "normal") {
-            return tasks.filter(task => task.completed === true && task.category === "normal")
+            return tasks.filter(task => task.completed === true && task.category === "normal" && task.title.toLowerCase().includes(searchs.toLowerCase()))
+        }
+        else if (filterByCategory === "shooping") {
+            return tasks.filter(task => task.completed === true && task.category === "shooping" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "food") {
-            return tasks.filter(task => task.completed === true && task.category === "food")
+            return tasks.filter(task => task.completed === true && task.category === "food" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
         else if (filterByCategory === "training") {
-            return tasks.filter(task => task.completed === true && task.category === "training")
+            return tasks.filter(task => task.completed === true && task.category === "training" && task.title.toLowerCase().includes(searchs.toLowerCase()))
         }
     }
 }
 
-/* 1 sposob alternatywa z AVAILABLE_FILTERS*/
-/* const filterTodos = (tasks, filters) => {
-    if (filters === AVAILABLE_FILTERS.SHOW_ALL) {
-        console.log(filters)
-        return tasks.filter(task => task);
-    }
-    else if (filters === AVAILABLE_FILTERS.SHOW_NOTCOMPLETED) {
-        return tasks.filter(task => task.completed === false);
-    }
-
-    else if (filters === AVAILABLE_FILTERS.SHOW_COMPLETED) {
-        return tasks.filter(task => task.completed === true);
-    }
-}
- */
-
-/* 2 sposob */
-/* const filterTodos = (tasks, filters) => {
-    switch (filters) {
-        case AVAILABLE_FILTERS.SHOW_ALL:
-            return tasks.filter(task => task.completed === false);
-        case AVAILABLE_FILTERS.SHOW_COMPLETED:
-            return tasks.filter(task => task.completed === true);
-        default:
-            return tasks
-    }
-
-} */
 
 const mapStateToProps = state => ({
-    //tasks: state.tasks,
-    //filters: state.filters
-    filterByCategory: state.filterByCategory,
-    tasks: filterTodos(state.tasks, state.filters, state.filterByCategory)
+    tasks: filterTasks(state.tasks, state.filters, state.filterByCategory, state.searchs)
 })
 export default connect(mapStateToProps)(TaskList);
-
-/* newCountriesList = actualCountriesList.filter(country => { //filtrowanie, jeśli jakiś kraj zawiera wpisaną frazę to go zwróć, toLowerCase() jest zastosowane, żeby porównywany kraj i wartość z inputa miały małe litery, żeby nie wystąpił konflikt
-                return country.name.toLowerCase().includes(e.target.value.toLocaleLowerCase());
-            }) */
