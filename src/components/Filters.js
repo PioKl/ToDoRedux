4320a023cb90f8ef1abeba8578df54dfb3ac5116
PioKl <kosiu342@gmail.com>
2020-05-26 21:36:42 +0200

    Filters.js| Condition if task is edited dont display filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { connect } from 'react-redux';
 import { showAll, showCompleted, showNotCompleted } from "../actions/filter.actions";
 import { showByCategory } from "../actions/filterByCategory.actions"
 import { searchTask } from "../actions/filterSearch.actions"
+import { EditContext } from '../contexts/EditContext';
 const Filters = ({ showAll, showCompleted, showNotCompleted, showByCategory, filters, searchTask }) => {
 
     const allCategories = "allCategories";
     const [categoryToFilter, setCategoryToFilter] = useState(allCategories);
     const [search, setSearch] = useState('')
+    const { isEdited } = useContext(EditContext);
 
     const handleAll = (e) => {
         showAll(e.target.value)
@@ -32,19 +34,22 @@ const Filters = ({ showAll, showCompleted, showNotCompleted, showByCategory, fil
     })
 
     return (
-        <div>
-            <input type="text" value={search} onChange={handleSearch} />
-            <button value="SHOW_ALL" disabled={filters === "SHOW_ALL" ? true : false} onClick={handleAll}>All</button>
-            <button value="SHOW_NOTCOMPLETED" disabled={filters === "SHOW_NOTCOMPLETED" ? true : false} onClick={handleNotCompleted}>Not Completed</button>
-            <button value="SHOW_COMPLETED" disabled={filters === "SHOW_COMPLETED" ? true : false} onClick={handleCompleted}>Completed</button>
-            <select id="categories">
-                <option onClick={handleCategory} defaultValue="selected" value={allCategories}>All Categories</option>
-                <option onClick={handleCategory} value="normal">Normal</option>
-                <option onClick={handleCategory} value="shopping">Shopping</option>
-                <option onClick={handleCategory} value="food">Food</option>
-                <option onClick={handleCategory} value="training">Training</option>
-            </select>
-        </div>
+        <>
+            {isEdited ? null : <div>
+                <input type="text" value={search} onChange={handleSearch} />
+                <button value="SHOW_ALL" disabled={filters === "SHOW_ALL" ? true : false} onClick={handleAll}>All</button>
+                <button value="SHOW_NOTCOMPLETED" disabled={filters === "SHOW_NOTCOMPLETED" ? true : false} onClick={handleNotCompleted}>Not Completed</button>
+                <button value="SHOW_COMPLETED" disabled={filters === "SHOW_COMPLETED" ? true : false} onClick={handleCompleted}>Completed</button>
+                <select id="categories">
+                    <option onClick={handleCategory} defaultValue="selected" value={allCategories}>All Categories</option>
+                    <option onClick={handleCategory} value="normal">Normal</option>
+                    <option onClick={handleCategory} value="shopping">Shopping</option>
+                    <option onClick={handleCategory} value="food">Food</option>
+                    <option onClick={handleCategory} value="training">Training</option>
+                </select>
+            </div>}
+
+        </>
     );
 }
 
