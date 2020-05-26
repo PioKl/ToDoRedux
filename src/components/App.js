25263cf0c654fea0e07f19d4bdfa4f8e1986d579
PioKl <kosiu342@gmail.com>
2020-05-26 21:33:33 +0200

    App.js|Add EditContextProvider to wrap all components

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,18 @@ import '../style/App.scss';
 import AddTask from './AddTask';
 import TaskList from './TaskList';
 import Filters from './Filters';
-
+import EditContextProvider from '../contexts/EditContext';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <main>
-        <Filters />
-        <AddTask />
-        <TaskList />
+        <EditContextProvider>
+          <Filters />
+          <AddTask />
+          <TaskList />
+        </EditContextProvider>
       </main>
     </div>
   );
