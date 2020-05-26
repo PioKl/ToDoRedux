a7bd212cc8a395ae2e5139d5a701ebea26931fb2
PioKl <kosiu342@gmail.com>
2020-05-26 21:31:23 +0200

    task.reducer.js| Add two new cases EDIT_TASK - display task which user wants to edit SAVE_EDIT_TASK - saves edited tasks and display tasks

--- src/reducers/task.reducer.js
+++ src/reducers/task.reducer.js
@@ -10,6 +10,7 @@ const tasks = (state = [], action) => {
                     description: action.description,
                     category: action.category,
                     completed: action.completed,
+                    edited: action.edited,
                 }
             ];
         case "REMOVE_TASK":
@@ -23,6 +24,20 @@ const tasks = (state = [], action) => {
                     ...task, //wszystko co znajduje sie w obiekcie zamiast przepisywac jak powyzej
                     completed: task.id === action.id ? !task.completed : task.completed
                 }
+            })
+        case "EDIT_TASK":
+            //{ ...task, edited: false } zeby zapobiec błędowi podczas odświeżenia w panelu edycji, jeśli odświeżę w trakcie panelu edycji i wejde w edycje kolejnego tasku, to w panelu mogą pojawić się dwa taski do edycji
+            return state.map((task) => task.id === action.id ? { ...task, edited: true } : { ...task, edited: false })
+        case "SAVE_EDIT_TASK":
+            return state.map((task) => {
+                if (task.id === action.id) {
+                    return {
+                        ...task,
+                        title: action.title,
+                        edited: false,
+                    }
+                }
+                else return task
             })
         default:
             return state;
