4b1995dfb16620646132a48ef3f222d8d37973db
PioKl <kosiu342@gmail.com>
2020-05-26 21:34:32 +0200

    AddTask.js| Condition if task is edited dont display addTask panel

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { connect } from "react-redux";
 import { addTask } from "../actions/task.actions";
+import { EditContext } from '../contexts/EditContext';
 
 const AddTask = ({ addTask }) => {
     const normal = "normal";
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState(normal);
+    const { isEdited } = useContext(EditContext);
 
     const handleTitle = (e) => {
         setTitle(e.target.value);
@@ -20,19 +22,24 @@ const AddTask = ({ addTask }) => {
         }
     }
     return (
-        <div>
-            <form onSubmit={handleAddTask}>
-                <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
-                <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
-                <select id="categories">
-                    <option defaultValue="selected" onClick={(e) => setCategory(e.target.value)} value={normal}>Normal</option>
-                    <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
-                    <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
-                    <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
-                </select>
-                <input type="submit" value="add task" />
-            </form>
-        </div>
+        <>
+            {isEdited ? null :
+                <div>
+                    <form onSubmit={handleAddTask}>
+                        <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
+                        <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
+                        <select id="categories">
+                            <option defaultValue="selected" onClick={(e) => setCategory(e.target.value)} value={normal}>Normal</option>
+                            <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
+                            <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
+                            <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
+                        </select>
+                        <input type="submit" value="add task" />
+                    </form>
+                </div>
+            }
+
+        </>
     );
 }
 
