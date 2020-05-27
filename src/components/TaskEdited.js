a91ae260eaa49cd89d383adfaedd84c167786b6e
PioKl <kosiu342@gmail.com>
2020-05-27 20:34:29 +0200

    TaskEdited.js|Add description and category to edit

--- src/components/TaskEdited.js
+++ src/components/TaskEdited.js
@@ -6,6 +6,8 @@ const Task = ({ task, saveEditTask }) => {
     const { title, description, category, id } = task;
     const { setIsEdited } = useContext(EditContext);
     const [editedTitle, setEditedTitle] = useState('');
+    const [editedDescription, setEditedDescription] = useState('');
+    const [editedCategory, setEditedCategory] = useState('');
 
     const handleEditedTitle = (e) => {
         setEditedTitle(e.target.value);
@@ -13,7 +15,7 @@ const Task = ({ task, saveEditTask }) => {
     const handleSaveEditedTask = (e) => {
         e.preventDefault();
         //editedTitle ? editedTitle : title - jeśli editedTitle nie jest pusty, czyl użytkownik coś zmienił to użyj nowego tytułu, a jeśli nie to niech dalej będzie używany stary
-        saveEditTask(id, editedTitle ? editedTitle : title);
+        saveEditTask(id, editedTitle ? editedTitle : title, editedDescription ? editedDescription : description, editedCategory ? editedCategory : category);
         setIsEdited(false)
     }
 
@@ -21,13 +23,20 @@ const Task = ({ task, saveEditTask }) => {
         <div>
             <form onSubmit={handleSaveEditedTask}>
                 <input type="text" placeholder={title} onChange={handleEditedTitle} defaultValue={title} name="" id="" />
+                <input type="text" maxLength="50" size="50" placeholder={description} defaultValue={description} onChange={(e) => setEditedDescription(e.target.value)} name="" id="" />
+                <select defaultValue={category} id="categories">
+                    <option onClick={(e) => setEditedCategory(e.target.value)} value="normal" >Normal</option>
+                    <option onClick={(e) => setEditedCategory(e.target.value)} value="shopping">Shopping</option>
+                    <option onClick={(e) => setEditedCategory(e.target.value)} value="food">Food</option>
+                    <option onClick={(e) => setEditedCategory(e.target.value)} value="training">Training</option>
+                </select>
                 <input type="submit" value="Save" />
             </form>
         </div>
     );
 }
 const mapDispatchToProps = dispatch => ({
-    saveEditTask: (id, title) => dispatch(saveEditTask(id, title))
+    saveEditTask: (id, title, description, category) => dispatch(saveEditTask(id, title, description, category))
 });
 
 
