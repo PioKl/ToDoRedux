f60d6d6b583394273b61fa0885dfa5520eb66ea9
PioKl <kosiu342@gmail.com>
2020-05-27 20:42:00 +0200

    AddTask.js|defaultValue modification in select

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,10 +4,10 @@ import { addTask } from "../actions/task.actions";
 import { EditContext } from '../contexts/EditContext';
 
 const AddTask = ({ addTask }) => {
-    const normal = "normal";
+    //const normal = "normal";
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-    const [category, setCategory] = useState(normal);
+    const [category, setCategory] = useState("normal");
     const { isEdited } = useContext(EditContext);
 
     const handleTitle = (e) => {
@@ -28,8 +28,8 @@ const AddTask = ({ addTask }) => {
                     <form onSubmit={handleAddTask}>
                         <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
                         <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
-                        <select id="categories">
-                            <option defaultValue="selected" onClick={(e) => setCategory(e.target.value)} value={normal}>Normal</option>
+                        <select defaultValue="normal" id="categories">
+                            <option onClick={(e) => setCategory(e.target.value)} value="normal">Normal</option>
                             <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
                             <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
                             <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
