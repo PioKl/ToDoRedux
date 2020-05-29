d39eb565100460d66b51ee39099fc1ad526d6dc3
PioKl <kosiu342@gmail.com>
2020-05-29 20:50:09 +0200

    AddTask.js|Add new buttons Cancel and Create New Task with functionality, also usage of CreateTaskContext (changing state) and comment about problem with chrome

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { connect } from "react-redux";
 import { addTask } from "../actions/task.actions";
 import { EditContext } from '../contexts/EditContext';
+import { CreateTaskContext } from '../contexts/CreateTaskContext';
 
 const AddTask = ({ addTask }) => {
     //const normal = "normal";
@@ -9,9 +10,13 @@ const AddTask = ({ addTask }) => {
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState("normal");
     const { isEdited } = useContext(EditContext);
+    const { isTaskCreated, setIsTaskCreated } = useContext(CreateTaskContext);
 
     const handleTitle = (e) => {
         setTitle(e.target.value);
+    }
+    const handleCreateTask = (e) => {
+        setIsTaskCreated(true);
     }
     const handleAddTask = (e) => {
         e.preventDefault();
@@ -19,24 +24,35 @@ const AddTask = ({ addTask }) => {
             addTask(title, description, category);
             setTitle("");
             setDescription("");
+            setIsTaskCreated(false);
         }
+    }
+    const handleCancel = () => {
+        setIsTaskCreated(false);
     }
     return (
         <>
             {isEdited ? null :
-                <div>
-                    <form onSubmit={handleAddTask}>
-                        <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
-                        <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
-                        <select defaultValue="normal" id="categories">
-                            <option onClick={(e) => setCategory(e.target.value)} value="normal">Normal</option>
-                            <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
-                            <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
-                            <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
-                        </select>
-                        <input type="submit" value="add task" />
-                    </form>
-                </div>
+                <>
+                    {isTaskCreated ? <div>
+                        <form onSubmit={handleAddTask}>
+                            <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
+                            <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
+                            <select defaultValue="normal" id="categories" /* do chroma zeby dzialalo trzeba tak uzyc onChange={(e) => setCategory(e.target.value)} */>
+                                <option onClick={(e) => setCategory(e.target.value)} value="normal">Normal</option>
+                                <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
+                                <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
+                                <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
+                            </select>
+                            <input type="submit" value="add task" />
+                        </form>
+                        <button onClick={handleCancel}>Cancel</button>
+                    </div>
+                        :
+                        <div>
+                            <button onClick={handleCreateTask}>Create New Task</button>
+                        </div>}
+                </>
             }
 
         </>
