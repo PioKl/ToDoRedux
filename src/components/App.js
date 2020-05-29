adb6dab6e5a0a3dbc262413eea7a12114c0cb281
PioKl <kosiu342@gmail.com>
2020-05-29 20:49:12 +0200

    App.js|Add CreateTaskContextProvider

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,20 @@ import AddTask from './AddTask';
 import TaskList from './TaskList';
 import Filters from './Filters';
 import EditContextProvider from '../contexts/EditContext';
+import CreateTaskContextProvider from '../contexts/CreateTaskContext';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
       <main>
-        <EditContextProvider>
-          <Filters />
-          <AddTask />
-          <TaskList />
-        </EditContextProvider>
+        <CreateTaskContextProvider>
+          <EditContextProvider>
+            <Filters />
+            <AddTask />
+            <TaskList />
+          </EditContextProvider>
+        </CreateTaskContextProvider>
       </main>
     </div>
   );
