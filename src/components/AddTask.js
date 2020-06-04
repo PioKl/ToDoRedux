412e669050c5df08abc99bae1878d9eef56e28bc
PioKl <kosiu342@gmail.com>
2020-06-04 19:54:12 +0200

    AddTask.js|Add new classes, headings, change from select to choose category to buttons

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -54,20 +54,48 @@ const AddTask = ({ addTask }) => {
         <>
             {isEdited ? null :
                 <>
-                    {isTaskCreated ? <div>
-                        <form onSubmit={handleAddTask}>
-                            <input type="text" placeholder="title" value={title} onChange={handleTitle} name="" id="" />
-                            <input type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
-                            <select defaultValue="normal" id="categories" /* do chroma zeby dzialalo trzeba tak uzyc onChange={(e) => setCategory(e.target.value)} */>
-                                <option onClick={(e) => setCategory(e.target.value)} value="normal">Normal</option>
-                                <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
-                                <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
-                                <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
-                            </select>
-                            <input type="submit" value="add task" />
-                        </form>
-                        <button onClick={handleCancel}>Cancel</button>
-                    </div>
+                    {isTaskCreated ?
+                        <div className="newTaskContainer">
+                            <form className="addTask" onSubmit={handleAddTask}>
+                                <h1 className="addTask__heading">Title</h1>
+                                <input className="addTask__input addTask__input--title" type="text" placeholder="title" value={title} onChange={handleTitle} required name="" id="" />
+                                <h1 className="addTask__heading">Description</h1>
+                                <textarea className="addTask__input addTask__input--description" type="text" maxLength="50" rows="3" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" />
+                                {/*                                 <input className="addTask__input addTask__input--description" type="text" maxLength="50" size="50" placeholder="description" value={description} onChange={(e) => setDescription(e.target.value)} name="" id="" /> */}
+                                <h1 className="addTask__heading">Category</h1>
+                                <div className="addTask__categoriesButtons">
+                                    <button
+                                        className={`addTask__categoryButton ${category === "normal" && "addTask__categoryButton--active"}`}
+                                        onClick={(e) => { e.preventDefault(); setCategory(e.target.value) }}
+                                        value="normal" title="Normal">Normal
+                                        </button>
+                                    <button
+                                        className={`addTask__categoryButton ${category === "shopping" && "addTask__categoryButton--active"}`}
+                                        onClick={(e) => { e.preventDefault(); setCategory(e.target.value) }}
+                                        value="shopping" title="Shopping">Shopping</button>
+                                    <button
+                                        className={`addTask__categoryButton ${category === "food" &&
+                                            "addTask__categoryButton--active"}`}
+                                        onClick={(e) => { e.preventDefault(); setCategory(e.target.value) }}
+                                        value="food" title="Food">Food</button>
+                                    <button
+                                        className={`addTask__categoryButton ${category === "training" && "addTask__categoryButton--active"}`}
+                                        onClick={(e) => { e.preventDefault(); setCategory(e.target.value) }}
+                                        value="training" title="Training">Training</button>
+                                </div>
+                                {/*                                 <select defaultValue="normal" id="categories" // do chroma zeby dzialalo trzeba tak uzyc onChange={(e) => setCategory(e.target.value)} //>
+                                    <option onClick={(e) => setCategory(e.target.value)} value="normal">Normal</option>
+                                    <option onClick={(e) => setCategory(e.target.value)} value="shopping">Shopping</option>
+                                    <option onClick={(e) => setCategory(e.target.value)} value="food">Food</option>
+                                    <option onClick={(e) => setCategory(e.target.value)} value="training">Training</option>
+                                </select> */}
+                                <div className="addTask__finishButtons">
+                                    <button className="addTask__finishButton addTask__finishButton--submit" type="submit" title="Add Task">Add Task</button>
+                                    {/* <input className="addTask__input addTask__input--submit" type="submit" value="add task" /> */}
+                                    <button className="addTask__finishButton addTask__finishButton--cancel" onClick={handleCancel} title="Cancel">Cancel</button>
+                                </div>
+                            </form>
+                        </div>
                         :
                         <div className="create-newTask">
                             <button title="Create New Task" className={`create-newTask__button ${scroll ? "create-newTask__button--scroll" : "create-newTask__button--standard"}`} onClick={handleCreateTask}>{scroll ? <i className="fas fa-plus create-newTask__button--plusIcon"></i> : "Create New Task"}</button>
