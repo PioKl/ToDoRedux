af70d758da41a97fd45d83fad26ba92ab61abf0c
PioKl <kosiu342@gmail.com>
2020-06-19 12:51:49 +0200

    Updates

---
+++ precache-manifest.39b3c8fbc2677eb710a9cfa98f412248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caa1d3be2498bea4f218b0f3cea8442f",
+    "url": "/ToDoRedux/index.html"
+  },
+  {
+    "revision": "e9943a562111fc121cf7",
+    "url": "/ToDoRedux/static/css/main.3a656547.chunk.css"
+  },
+  {
+    "revision": "17635693afbc109d497f",
+    "url": "/ToDoRedux/static/js/2.4351cfdd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDoRedux/static/js/2.4351cfdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9943a562111fc121cf7",
+    "url": "/ToDoRedux/static/js/main.edd98f88.chunk.js"
+  },
+  {
+    "revision": "8ec7ba6028425c429803",
+    "url": "/ToDoRedux/static/js/runtime-main.b3981d73.js"
+  }
+]);
